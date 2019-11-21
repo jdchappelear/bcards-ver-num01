d2cea3f9da398a4669d1c10752c7825362b1b598
John Chappelear <john.chappelear@gmail.com>
2019-11-20 20:48:43 -0500

    Simple Sign Up completed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,13 @@
+import * as firebase from 'firebase';
+
 export class AuthService {
+    signupUser(email: string, password: string) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(
+                error => console.log(error)
+            )
+    }
+
     loggedIn = false;
 
     isAuthenticated() {
