c2a4a0197d3c78a46de8d29d30d22eca83a56e03
John Chappelear <john.chappelear@gmail.com>
2019-11-25 20:13:23 -0500

    Images now are save and displayed with the text from the DB

--- src/app/businesscard/businesscard.component.ts
+++ src/app/businesscard/businesscard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { Customer } from '../customer';
 
 @Component({
   selector: 'app-businesscard',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinesscardComponent implements OnInit {
 
-  constructor() { }
+  @Input() customer: Customer;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
+  }
+
+   
+  deleteCustomer() {
+    this.customerService
+      .deleteCustomer(this.customer.key)
+      .catch(err => console.log(err));
   }
 
 }
--- src/app/businesscards/businesscards.component.ts
+++ src/app/businesscards/businesscards.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-
+import { CustomerService } from '../customer.service';
+import { map } from 'rxjs/operators';
+ 
 @Component({
   selector: 'app-businesscards',
   templateUrl: './businesscards.component.html',
@@ -8,13 +10,31 @@ import { AuthService } from '../auth.service';
 })
 export class BusinesscardsComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  customers: any;
+
+  constructor(private authService: AuthService, private customerService: CustomerService) { }
 
   onLogout() {
     this.authService.logout();
   }
 
   ngOnInit() {
+    this.getCustomersList();
   }
-
+  getCustomersList() {
+    this.customerService.getCustomersList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(customers => {
+      this.customers = customers;
+    });
+  }
+ 
+  deleteCustomers() {
+    this.customerService.deleteAll();
+  }
+ 
 }
